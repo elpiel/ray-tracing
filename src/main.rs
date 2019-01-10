26d03ed92a11eefbf15b82993df2827bf98da60c
Lachezar Lechev <elpiel93@gmail.com>
2019-01-10 12:16:55 +0100

    Main - use Ray & Vec3 and make gradient and center a camera in the middle

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-use ray_tracing::Vec3;
 use std::fs::File;
 use std::io::LineWriter;
 use std::io::prelude::*;
 
-fn main() -> Result<(), std::io::Error>  {
+use ray_tracing::Ray;
+use ray_tracing::Vec3;
+
+fn color(ray: &Ray) -> Vec3 {
+    let unit_direction = Vec3::unit_vector(ray.direction);
+    let t = 0.5 * (unit_direction.y() + 1.0);
+
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+}
+
+fn main() -> Result<(), std::io::Error> {
     let width = 200;
     let height = 100;
     let max_color = 255;
@@ -14,13 +23,23 @@ fn main() -> Result<(), std::io::Error>  {
     let beginning_file = format!("P3\n{} {}\n{}\n", width, height, max_color);
     file.write_all(beginning_file.as_bytes())?;
 
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+
     for j in (0..height).rev() {
         for i in 0..width {
-            let vec3 = Vec3::new(i as f64 / width as f64, j as f64 / height as f64,  0.2);
+            let u = i as f64 / width as f64;
+            let v = j as f64 / height as f64;
+            let destination = lower_left_corner + u * horizontal + v * vertical;
 
-            let ir = (255.99 * vec3[0]) as i32;
-            let ig = (255.99 * vec3[1]) as i32;
-            let ib = (255.99 * vec3[2]) as i32;
+            let ray = Ray::new(origin, destination);
+            let col = color(&ray);
+
+            let ir = (255.99 * col[0]) as i32;
+            let ig = (255.99 * col[1]) as i32;
+            let ib = (255.99 * col[2]) as i32;
 
             let pixel_color = format!("{} {} {}\n", ir, ig, ib);
             file.write_all(pixel_color.as_bytes())?;

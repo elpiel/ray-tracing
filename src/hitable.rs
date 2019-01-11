eb5709ab20dec5581ad5b588e2cfa9a0ab164774
Lachezar Lechev <elpiel93@gmail.com>
2019-01-11 12:24:59 +0100

    hitable - Add HitRecord and Hitable trait

---
+++ src/hitable.rs
@@ -0,0 +1,18 @@
+use crate::ray::Ray;
+use crate::Vec3;
+
+pub struct HitRecord {
+    t: f64,
+    p: Vec3,
+    normal: Vec3,
+}
+
+impl HitRecord {
+    pub fn main(t: f64, p: Vec3, normal: Vec3) -> Self {
+        Self { t, p, normal }
+    }
+}
+
+pub trait Hitable {
+    fn hit(ray: &Ray, t_min: f64, t_max: f64, hit_record: &HitRecord ) -> bool;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod vec3;
 mod ray;
+mod hitable;
 
 pub use self::vec3::Vec3;
 pub use self::ray::Ray;
-
-
+pub use self::hitable::*;
